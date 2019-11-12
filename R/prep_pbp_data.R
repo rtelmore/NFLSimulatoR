0d331e529140c740e852e0fb4f395c37bfdf9c00
Ryan Elmore <Ryan.Elmore@du.edu>
2019-11-12 15:14:29 -0700

    prepping data function better

--- R/prep_pbp_data.R
+++ R/prep_pbp_data.R
@@ -10,17 +10,20 @@
 #' dt <- prep_pbp_data(nflscrapr_pbp_data)
 #' }
 prep_pbp_data <- function(nflscrapR_data){
-  dt <- data.table::setDT(nflscrapR_data)
-  dt[, ":="
-     (p_diff = total_home_score - total_away_score,
-       is_two_point = grepl("TWO-POINT CONVERSION", desc),
-       is_fumble = !is.na(fumble_recovery_1_team),
-       is_td_offense = rush_touchdown | pass_touchdown,
-       is_field_goal = field_goal_attempt & (field_goal_result == "made"),
-       yfog = 100 - yardline_100,
-       touchback = ifelse(grepl("Touchback", desc), 1, 0),
-       punt_attempt = as.numeric(punt_attempt))][
-         , ":=" (is_turnover = grepl("INTERCEPTED", desc) |
-         (is_fumble & fumble_recovery_1_team != posteam))]
-  return(dt[!is.na(yards_gained) & !is_two_point & !is.na(punt_attempt)])
+  dt <- data.table::setDT(nflscrapR_data)[
+    , ":="
+    (p_diff = total_home_score - total_away_score,
+      is_two_point = grepl("TWO-POINT CONVERSION", desc),
+      is_fumble = !is.na(fumble_recovery_1_team),
+      is_td_offense = rush_touchdown | pass_touchdown,
+      is_field_goal = field_goal_attempt & (field_goal_result == "made"),
+      yfog = 100 - yardline_100,
+      touchback = ifelse(grepl("Touchback", desc), 1, 0),
+      punt_attempt = as.numeric(punt_attempt))][
+        , ":=" (is_turnover = grepl("INTERCEPTED", desc) |
+                  (is_fumble & fumble_recovery_1_team != posteam))][
+                    !is.na(yards_gained)][
+                      is_two_point == FALSE][
+                        !is.na(punt_attempt)]
+  return(dt)
 }
