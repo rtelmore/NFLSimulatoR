fe9513ad5103d7065ca08478c4fda5bd71a150e6
Ben <benjamin@smu.edu>
2020-03-31 16:50:30 -0600

    -Edit the number of observations in the 2018 dataset from nflscrapR -Add in code for the 2019 regular season data

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 #' NFL Play-by-Play Data 2018
 #'
-#' A dataset containing the 256 attributes associated with 45143 plays in the
+#' A dataset containing the 256 attributes associated with 45156 plays in the
 #' NFL from the 2018 regular season. The data were obtained using the nflscrapR package.
 #'
-#' @format A data frame with 45143 rows and 256 variables:
+#' @format A data frame with 45156 rows and 256 variables:
 #' \describe{
 #'   \item{play_id}{id within a game}
 #'   \item{game_id}{id of the specific game}
@@ -14,3 +14,20 @@
 #' }
 #' @source \url{https://ryurko.github.io/nflscrapR-data/}
 "reg_pbp_2018"
+
+#' NFL Play-by-Play Data 2018
+#'
+#' A dataset containing the 256 attributes associated with 45546 plays in the
+#' NFL from the 2018 regular season. The data were obtained using the nflscrapR package.
+#'
+#' @format A data frame with 45546 rows and 256 variables:
+#' \describe{
+#'   \item{play_id}{id within a game}
+#'   \item{game_id}{id of the specific game}
+#'   \item{home_team}{home team playing}
+#'   \item{away_team}{away team playing}
+#'   \item{posteam}{team with possession}
+#'   ...
+#' }
+#' @source \url{https://ryurko.github.io/nflscrapR-data/}
+"reg_pbp_2019"
